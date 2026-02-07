b5dde4f23698f8770cc7c796d45cf27bcd58d6b1
Orca Nox <57429813+orca-nox@users.noreply.github.com>
2026-02-08 02:17:00 +0900

    add ui update

--- Assets/Scripts/InfoGatherPhase/BattleDecisionUI.cs
+++ Assets/Scripts/InfoGatherPhase/BattleDecisionUI.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 namespace InfoGatherPhase
 {
@@ -9,14 +10,18 @@ namespace InfoGatherPhase
         [SerializeField] private GameObject panel;
         [SerializeField] private Button confrontButton;
         [SerializeField] private Button declineButton;
+        [SerializeField] private TextMeshProUGUI declineLabel;
 
         public event Action OnConfront;
         public event Action OnDecline;
 
         public bool IsOpen { get; private set; }
 
+        private int totalStageItems;
+
         private void Awake()
         {
+            totalStageItems = FindObjectsByType<ClickableItem>(FindObjectsSortMode.None).Length;
             confrontButton.onClick.AddListener(HandleConfront);
             declineButton.onClick.AddListener(HandleDecline);
             panel.SetActive(false);
@@ -30,6 +35,7 @@ namespace InfoGatherPhase
 
         public void Show()
         {
+            UpdateDeclineLabel();
             panel.SetActive(true);
             IsOpen = true;
         }
@@ -50,6 +56,21 @@ namespace InfoGatherPhase
         {
             Hide();
             OnDecline?.Invoke();
+        }
+
+        private void UpdateDeclineLabel()
+        {
+            if (declineLabel == null) return;
+
+            int collected = GameManager.Instance != null
+                ? GameManager.Instance.CollectedItems.Count
+                : 0;
+
+            bool allCollected = collected >= totalStageItems;
+
+            declineLabel.text = allCollected
+                ? $"Not yet... ({collected}/{totalStageItems} found)"
+                : $"Not yet...\n<color=red>You haven't found all items! ({collected}/{totalStageItems} found)</color>";
         }
     }
 }
